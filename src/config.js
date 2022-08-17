399c801d5df102b89d6d714a6b649138ee62c61f
CrystalBlockDev <crystal.blockdev@gmail.com>
2022-08-17 14:54:11 +0200

    updated for geting claimavble amount

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
-
 export const vestingTokenAddress = "b02ec9fe439a945bcc0cc4a786f22fab7ae41829e10ea029e6f82af1b3833b60";
 export const vestingTokenSymbol = "ACME";
-export const vestingContractAddress = "8213d6c189d6c90a981d6cc4082c4e902370d24c8575ee72d78e7d841ffd0ae1";
-export const vestingContractPackageHash = "b76fb68884e5d2ad836e11abdec4e03d3218b45e551c738914fcbf5c4c64228b";
+export const vestingContractAddress = "d8757c139cf8a321d79dc36fe17bea029288d9faf8fd33b593b9f1c402481e22";
+export const vestingContractPackageHash = "285448f572fa7f3801a649ef177939895eb4da303216ab0a5ee64a9fbe692b5c";
 export const urfOfUserInfo = "uref-3242c4f045db5550bf8f8c06b19c1f2fe77909bb17f266a6b20161e10cc8985f-007";
